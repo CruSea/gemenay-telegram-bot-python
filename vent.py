44185a75c8e33578f9c3faf25ec8188baef35b5a
unknown <danwondimu00@gmail.com>
2020-10-22 11:32:15 +0200

    exeption handling

--- vent.py
+++ vent.py
@@ -78,59 +78,66 @@ def extract_unique_code(text):
     return text.split()[1] if len(text.split()) > 1 else None
 
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
-    uid = message.chat.id
-    userStep[uid] = 0
-    unique_code = extract_unique_code(message.text)
-    if unique_code:
-        issue=Api.getIssue(unique_code)
-        bot.send_message(uid,str(issue),reply_markup = buttonComment(unique_code))
-    else:
-        bot.send_message(uid,"Welcome to vent gemenaye",reply_markup = buttonStart())
-
+try:
+    def send_welcome(message):
+        uid = message.chat.id
+        userStep[uid] = 0
+        unique_code = extract_unique_code(message.text)
+        if unique_code:
+            issue=Api.getIssue(unique_code)
+            bot.send_message(uid,str(issue),reply_markup = buttonComment(unique_code))
+        else:
+            bot.send_message(uid,"Welcome to vent gemenaye",reply_markup = buttonStart())
+except:
+    pass
 
 
 try:
 
     @bot.callback_query_handler(func=lambda call: True)
     def callback_query(call):
-        uid=call.from_user.id
-        text = str(call.data)
-        print(text)
-        text = text.split("--")
-        if text[0] == "Add_a_comment":
-            bot.answer_callback_query(call.id, "send your comment")
-            bot.send_message(uid,"Add comment...",reply_markup = cancel())
-            issueIds[0] = int(text[1]);
-            print(issueIds[0])
-            userStep[uid] = 2
-        elif text[0] == "Browse_comments":
-            #issueIds[0] = int(text[1]);
-            #print(call)
-            bot.answer_callback_query(call.id, "comment coming up")
-            comm=Api.bComment(text[1])
-            try:
-                print(comm)
-                i=0
-                for ids in comm:
-                    bot.send_message(uid, comm[i]['comment'],reply_markup= like())
-                    i=i+1
-            except:
-                bot.send_message(uid,"No comment.....",reply_markup = buttonStart())
-
+        try:
+            uid=call.from_user.id
+            text = str(call.data)
+            print(text)
+            text = text.split("--")
+            if text[0] == "Add_a_comment":
+                bot.answer_callback_query(call.id, "send your comment")
+                bot.send_message(uid,"Add comment...",reply_markup = cancel())
+                issueIds[0] = int(text[1]);
+                print(issueIds[0])
+                userStep[uid] = 2
+            elif text[0] == "Browse_comments":
+                #issueIds[0] = int(text[1]);
+                #print(call)
+                bot.answer_callback_query(call.id, "comment coming up")
+                comm=Api.bComment(text[1])
+                try:
+                    print(comm)
+                    i=0
+                    for ids in comm:
+                        bot.send_message(uid, comm[i]['comment'],reply_markup= like())
+                        i=i+1
+                except:
+                    bot.send_message(uid,"No comment.....",reply_markup = buttonStart())
+        except:
+            pass
     @bot.message_handler(func=lambda message: get_user_step(message.chat.id) == 1)
     def submitIssue(m):
-        uid = m.chat.id
-        text = m.text
-        if text == '❌ Cancel' :
-            bot.send_message(uid,"cancelled!",reply_markup = buttonStart())
-            userStep[uid] = 0
-        else:
-            print(text + '  s')
-            Api.addIssue(issue = text, user_id = uid)
-            bot.send_message(uid,"issue added! wait for approval",reply_markup = buttonStart())
-            bot.send_message(uid,"Do You need MENTOR",reply_markup = mentor())
-            userStep[uid] = 0
+        try:
+            uid = m.chat.id
+            text = m.text
+            if text == '❌ Cancel' :
+                bot.send_message(uid,"cancelled!",reply_markup = buttonStart())
+                userStep[uid] = 0
+            else:
+                print(text + '  s')
+                Api.addIssue(issue = text, user_id = uid)
+                bot.send_message(uid,"issue added! wait for approval",reply_markup = buttonStart())
+                bot.send_message(uid,"Do You need MENTOR",reply_markup = mentor())
+                userStep[uid] = 0
+        except:
+            pass
 
     @bot.message_handler(func=lambda message: get_user_step(message.chat.id) == 2)
     def addComment(m):
@@ -157,12 +164,15 @@ try:
 
     @bot.message_handler(func=lambda message: True, content_types=['text'])
     def command_default(m):
-        uid = m.chat.id
-        text = m.text
-        if text == "🔆 Start a Vent":
-            bot.send_message(uid, "Please send your issue.",reply_markup = cancel())
-            userStep[uid] = 1
-        #elif text=="":
+        try:
+            uid = m.chat.id
+            text = m.text
+            if text == "🔆 Start a Vent":
+                bot.send_message(uid, "Please send your issue.",reply_markup = cancel())
+                userStep[uid] = 1
+            #elif text=="":
+        except:
+            pass
 except Exception as identifier:
         print("error")
 
